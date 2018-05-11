18eace9528fdf25f04dad815924e31562742c8a2
nikita-skobov <nskobov.websites@gmail.com>
2018-05-11 14:27:01 +0000

    adds webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src/entry.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
