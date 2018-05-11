b6114c7ef9545d4b09e1d81b53c2c0f7a263561b
nikita-skobov <nskobov.websites@gmail.com>
2018-05-11 15:14:00 +0000

    adds express static routing

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const port           = 8082 //change this as needed
 
 app.use(express.json())
 app.use(helmet())
+app.use("/test1/static", express.static(__dirname + '/dist'))
+// test1 is the path in nginx that routes to port 8082. for other projects, test1 is not necessary.
 
 server.listen(port, () => {
   console.log('listening on port:', port)
+})
+
+app.get('/test1', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
 })
